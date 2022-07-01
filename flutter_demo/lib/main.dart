95257c8e8efacc1ee980044bcc3324b7a7436120
mwzhous <fxmingwei@foxmail.com>
2022-07-01 12:06:38 +0800

    flutter学习（3）

---
+++ flutter_demo/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo/bottom/index.dart';
+import 'package:flutter_demo/common/routes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // 以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前
+    // MaterialApp组件会覆盖掉这个值。
+    // SystemUiOverlayStyle systemUiOverlayStyle = const SystemUiOverlayStyle(
+    //   statusBarColor: Colors.transparent,
+    //   statusBarBrightness: Brightness.light,
+    // );
+    // SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      routes: routes,
+      initialRoute: '/',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const Index(),
+    );
+  }
+}
